e7678a15d93ed0b699624acce5a7827309b8a27d
Fernando <fernandodarolt@hotmail.com>
2022-10-19 19:57:03 -0300

    Incrementado validações

--- form_veiculo.php
+++ form_veiculo.php
@@ -38,6 +38,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Veículo</title>
+    <header class="header">
+        <nav>
+            <ul class="menu">
+                <li>Home</li>
+                <li>Cursos</li>
+                <li>Contatos</li>
+                <li>Escitórios</li>
+                <li>Localização</li>
+
+            </ul>
+        </nav>
+    </header>
 </head>
 <body>
     <form action="crud_veiculo.php" method="POST">
@@ -91,7 +103,7 @@
                 <br/>
                 <label for="chassi">Chassi</label>
                 <br/>
-                <input type="text" id="chassi" name="chassi" value="<?=$chassi?>"/>
+                <input type="text" id="chassi" name="chassi" maxlength="17" value="<?=$chassi?>"/>
             </div>
             <div>
                 <br/>
@@ -103,31 +115,31 @@
                 <br/>
                 <label for="potencia">Potência</label>
                 <br/>
-                <input type="text" id="potencia" name="potencia" value="<?=$potencia?>"/>
+                <input type="number" id="potencia" min="0" max="3000" name="potencia" value="<?=$potencia?>"/>
             </div>
             <div>
                 <br/>
                 <label for="cilindrada">Cilindrada</label>
                 <br/>
-                <input type="text" id="cilindrada" name="cilindrada" value="<?=$cilindrada?>"/>
+                <input type="number" id="cilindrada" min="0" max="9999" name="cilindrada" value="<?=$cilindrada?>"/>
             </div>
             <div>
                 <br/>
                 <label for="lotacao">Passageiros</label>
                 <br/>
-                <input type="text" id="lotacao" name="lotacao" value="<?=$lotacao?>"/>
+                <input type="number" id="lotacao" min="0" max="999" name="lotacao" value="<?=$lotacao?>"/>
             </div>
             <div>
                 <br/>
                 <label for="ano_fabricacao">Ano Fabricacao</label>
                 <br/>
-                <input type="text" id="ano_fabricacao" name="ano_fabricacao" value="<?=$ano_fabricacao?>"/>
+                <input type="number" id="ano_fabricacao" min="1800" max="2050"  name="ano_fabricacao" value="<?=$ano_fabricacao?>"/>
             </div>
             <div>
                 <br/>
                 <label for="ano_modelo">Ano Modelo</label>
                 <br/>
-                <input type="text" id="ano_modelo" name="ano_modelo" value="<?=$ano_modelo?>"/>
+                <input type="number" id="ano_modelo" min="1800" max="2050" name="ano_modelo" value="<?=$ano_modelo?>"/>
             </div>
             <div>
                 <br/>
